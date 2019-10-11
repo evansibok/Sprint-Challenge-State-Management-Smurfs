8b0d019a0d4e508dc6b7c9a01ccd44beaa380f26
evansibok <evansibok@gmail.com>
2019-10-11 08:47:18 +0100

    started working on project

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
 import "./index.css";
 import App from "./components/App";
+
+const store = createStore()
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));

495342d106e0e49d75e42633675a109a68a45b2e
Evans Ibok <evansibok@gmail.com>
2019-12-13 12:33:48 +0100

    add key to SmurfCard

--- smurfs/src/components/smurfCard.jsx
+++ smurfs/src/components/smurfCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SmurfCard = ({smurf}) => {
   return (
-    <div className="smurfCard">
+    <div className="smurfCard" key={smurf.id}>
       <h2>{smurf.name}</h2>
       <h4>Age: {smurf.age}</h4>
       <h4>Height: {smurf.height}</h4>
